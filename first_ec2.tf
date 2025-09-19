08bf5719ec2309579703af9653901e03fdc75d6f
Vamsi <vamsiraghumandala09@gmail.com>
2025-09-19 22:24:51 +0530

    first commit

---
+++ first_ec2.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.14.0"
+    }
+  }
+}
+
+provider "aws" {
+
+  # Configuration options
+}
+
+resource "aws_instance" "My_first_IAC_server" {
+  ami = "ami-08982f1c5bf93d976"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-0371e47388624e3e8"
+
+  tags = {
+    Name = "My First EC2 Server"
+    Project = "Terraform-Learning"
+    Owner = "Vamsi"
+  }
+}
+
+
+
